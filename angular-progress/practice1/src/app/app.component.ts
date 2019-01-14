269f99159992deb62a6cf0dda6fcf754d817b861
arielnykanen <arielnyken3n@gmail.com>
2019-01-14 20:18:33 +0200

    adding localstorage for  temperory db

--- angular-progress/practice1/src/app/app.component.ts
+++ angular-progress/practice1/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventModel } from './models/event.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'School Events';
+  constructor() {}
+  ngOnInit() {
+    const savedEvents: EventModel[] = [
+      {name:'Event1', date:'12.12.12', duration:'12.12.13', classes: [{  name: 'c' }] },
+      {name:'Event1', date:'12.12.12', duration:'12.12.13', classes: [{  name: 'd' }] },
+      {name:'Event1', date:'12.12.12', duration:'12.12.13', classes: [{  name: 'a' }] },
+      {name:'Event1', date:'12.12.12', duration:'12.12.13', classes: [{  name: 'e' }] },
+    ];
+    localStorage.setItem('events', JSON.stringify(savedEvents));
+  }
 }
+
