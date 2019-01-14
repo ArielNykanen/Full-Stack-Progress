d66ed09d44bb915ea72763f183749df3135abbb5
arielnykanen <arielnyken3n@gmail.com>
2019-01-14 21:25:04 +0200

    moving get from local to localstorage service

--- angular-progress/practice1/src/app/app.component.ts
+++ angular-progress/practice1/src/app/app.component.ts
@@ -10,13 +10,6 @@ export class AppComponent implements OnInit {
   title = 'School Events';
   constructor() {}
   ngOnInit() {
-    const savedEvents: EventModel[] = [
-      {name:'Event1', date:'12.12.12', duration:'12.12.13', classes: [{  name: 'c' }] },
-      {name:'Event1', date:'12.12.12', duration:'12.12.13', classes: [{  name: 'd' }] },
-      {name:'Event1', date:'12.12.12', duration:'12.12.13', classes: [{  name: 'a' }] },
-      {name:'Event1', date:'12.12.12', duration:'12.12.13', classes: [{  name: 'e' }] },
-    ];
-    localStorage.setItem('events', JSON.stringify(savedEvents));
   }
 }
 
