ad2e0e34b14184852a7ca0657522e625c85e76c8
arielnykanen <arielnyken3n@gmail.com>
2019-01-14 21:25:37 +0200

    setting up appModule

--- angular-progress/practice1/src/app/app.module.ts
+++ angular-progress/practice1/src/app/app.module.ts
@@ -4,17 +4,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SchoolEventsComponent } from './school-events/school-events.component';
+import { AddEventComponent } from './add-event/add-event.component';
+import { EventService } from './services/event.service';
+import { LocalStorageService } from './services/local-storage.service';
+import { ClassService } from './services/class.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SchoolEventsComponent
+    SchoolEventsComponent,
+    AddEventComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    EventService,
+  LocalStorageService,
+  ClassService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
