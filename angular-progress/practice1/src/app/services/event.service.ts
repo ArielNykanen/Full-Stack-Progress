b8dd2d64cac8c741a62f3734f656a2768996a8f4
arielnykanen <arielnyken3n@gmail.com>
2019-01-14 21:29:04 +0200

    injecting storage services + getAll method

--- angular-progress/practice1/src/app/services/event.service.ts
+++ angular-progress/practice1/src/app/services/event.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable()
 export class EventService {
 
-  constructor() { }
+  constructor(private storageServices: LocalStorageService) {}
+  getAll() {
+  return this.storageServices.get();
+  }
 
 }
